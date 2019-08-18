ed3f07de838803a878fc97312180424ee7fd35db
Lucas Damazio da Costa <lucas.damaziocosta@outlook.com>
2019-08-18 01:03:22 -0300

    Renaming builder.zsh to build.zsh

--- build.zsh
+++ build.zsh
@@ -2,7 +2,7 @@ alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSP
 
 alias grun='java -Xmx500M -cp "/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
 
-export CLASSPATH="/usr/local/lib/antlr-4.7.1-complete.jar;~/bin:$CLASSPATH"
+export CLASSPATH="/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH"
 
 antlr4 RUBY.g4
 javac *.java
