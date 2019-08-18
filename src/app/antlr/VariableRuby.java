dc555679128b780d762e0652abdd5a8324ff2896
Lucas Damazio da Costa <lucas.damaziocosta@outlook.com>
2019-08-18 20:47:29 -0300

    Adding VariableRuby class.

---
+++ src/app/antlr/VariableRuby.java
@@ -0,0 +1,6 @@
+import app.scopes.RubySymbol;
+import app.scopes.RubySymbol.Type;
+
+public class VariableRuby extends RubySymbol {
+    public VariableRuby(String name, Type type) { super(name, type); }
+}
