693371b9739eac62589687bd97e13bb0351d9af5
Lucas Damazio da Costa <lucas.damaziocosta@outlook.com>
2019-08-18 17:10:19 -0300

    Adding main class of project.

--- src/app/App.java
+++ src/app/App.java
@@ -1,9 +1,28 @@
 package app;
 
-import org.antlr.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import app.antlr.RUBYBaseListener;
+import app.antlr.RUBYLexer;
+import app.antlr.RUBYParser;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        ANTLRFileStream input = new ANTLRFileStream("teste.rb");
+
+        RUBYLexer lexer = new RUBYLexer(input);
+
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        RUBYParser parser = new RUBYParser(tokens);
+
+        RUBYBaseListener listener = new RUBYBaseListener();
+
+        ParseTree tree = parser.program();
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+
+        walker.walk(listener, tree);
     }
 }
