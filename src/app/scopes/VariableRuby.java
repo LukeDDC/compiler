35d18ac0d28c97511cb449a5f94c1f6467fa2b31
Lucas Damazio da Costa <lucas.damaziocosta@outlook.com>
2019-08-18 23:32:18 -0300

    feat(VariableRuby): Removing unecessary imports

--- src/app/scopes/VariableRuby.java
+++ src/app/scopes/VariableRuby.java
@@ -1,8 +1,5 @@
 package app.scopes;
 
-import app.scopes.RubySymbol;
-import app.scopes.RubySymbol.Type;
-
 public class VariableRuby extends RubySymbol {
     public VariableRuby(String name, Type type) { super(name, type); }
 }
