f908a8511153ba87bdef377d683704b30357b116
Lucas Damazio da Costa <lucas.damaziocosta@outlook.com>
2019-08-18 23:07:52 -0300

    Adding error handling for wrong assigments

--- src/app/antlr/ReferencePhase.java
+++ src/app/antlr/ReferencePhase.java
@@ -1,11 +1,13 @@
 package app.antlr;
 
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTreeProperty;
 
 import app.scopes.FunctionRuby;
-import app.scopes.RubySymbol;
 import app.scopes.GlobalScope;
+import app.scopes.RubySymbol;
 import app.scopes.Scope;
+import app.scopes.VariableRuby;
 
 /**
  * ReferencePhase
@@ -58,14 +60,45 @@ public class ReferencePhase extends RUBYBaseListener {
     currentScope = currentScope.getEnclosingScope();
   }
 
-	@Override public void exitVariable(RUBYParser.VariableContext ctx) {
+  @Override
+  public void exitVariable(RUBYParser.VariableContext ctx) {
     String name = ctx.ID().getSymbol().getText();
     RubySymbol var = currentScope.resolve(name);
-    if ( var==null ) {
-      // CheckSymbols.error(ctx.ID().getSymbol(), "no such variable: "+name);
+
+    if (var == null) {
+      error(ctx.ID().getSymbol(), name + " no such variable:");
     }
-    if ( var instanceof FunctionRuby) {
-        // CheckSymbols.error(ctx.ID().getSymbol(), name+" is not a variable");
+    if (var instanceof FunctionRuby){
+      error(ctx.ID().getSymbol(), name + " is not a variable");
     }
+  }
+
+  @Override public void exitAssign(RUBYParser.AssignContext ctx) {
+    String name = ctx.ID().getSymbol().getText();
+    RubySymbol var = currentScope.resolve(name);
+
+    if (var == null) {
+      error(ctx.ID().getSymbol(), name + " no such variable:");
+    }
+    if (var instanceof FunctionRuby){
+      error(ctx.ID().getSymbol(), name + " is not a variable");
+    }
+  }
+
+  @Override
+  public void exitFunctionCall(RUBYParser.FunctionCallContext ctx) {
+    String functionName = ctx.ID().getText();
+    RubySymbol meth = currentScope.resolve(functionName);
+
+    if (meth == null) {
+      error(ctx.ID().getSymbol(), functionName + " no such function:");
+    }
+    if (meth instanceof VariableRuby){
+      error(ctx.ID().getSymbol(), functionName + " is not a function");
+    }
+  }
+
+  public static void error(Token t, String msg) {
+    System.err.printf("line %d:%d %s\n", t.getLine(), t.getCharPositionInLine(), msg);
   }
 }
